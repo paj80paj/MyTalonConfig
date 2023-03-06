12512ed0878e20e476badac61d974fe8700cd398
paj80paj <102916005+paj80paj@users.noreply.github.com>
2023-03-06 11:15:59 +0000

    remove_punctuation

--- rem_punc_leading.py
+++ rem_punc_leading.py
@@ -1,4 +1,5 @@
 from talon import Module
+import string
 
 mod = Module()
 
@@ -6,7 +7,10 @@ mod = Module()
 class Actions:
     def remove_leading_punctuation(input_string: str) -> str:
         """removes leading punctuation from the supplied string"""
-        print(f"From function: {input_string=}")
         while input_string and input_string[0].isalnum() == False:
             input_string = input_string[1:]
-        return input_string 
+        return input_string
+    def remove_punctuation(text: str) -> str:
+        """remove punctuation from the supplied string using the string module"""
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        return text
